3b8664835c5b02fe7a65b3b1a9d0662a357e3c12
KelvinKoech <koechkelvin2001@gmail.com>
2023-01-19 17:47:55 +0300

    asynchronous

--- async.js
+++ async.js
@@ -5,13 +5,21 @@
     data.map((person)=>{
        let p = document.createElement('p');
        p.innerText = person.id;
-       document.body.appendChild(p);
+    //    document.body.appendChild(p);
        let k = document.createElement('p');
        k.innerText = person.title;
-       document.body.appendChild(k);
+    //    document.body.appendChild(k);
        let f = document.createElement('p');
        f.innerText = person.completed;
-       document.body.appendChild(f);
+    //    document.body.appendChild(f);
+
+
+    const div =document.createElement("div")
+    div.appendChild(p)
+    div.appendChild(k)
+    div.appendChild(f)
+    div.className ="grammy"
+    document.body.appendChild(div)
 
     })
     console.log(data)
